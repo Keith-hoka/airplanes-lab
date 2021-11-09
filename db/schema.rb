89705c00da95e9e95e4f7bf52defdce1c60f139a
Keith_hoka <keith_d_hau@hotmail.com>
2021-11-10 00:39:42 +1100

    added admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_131348) do
+ActiveRecord::Schema.define(version: 2021_11_09_133418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_131348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
