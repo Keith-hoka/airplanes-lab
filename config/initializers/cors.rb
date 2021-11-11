2dbd42b51f1055b27d35d0476883e2a863556546
BernardoMar <bernardomartin92@outlook.com>
2021-11-11 13:16:16 +1100

    Please work

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-<<<<<<< HEAD
-    origins '*'
-    resource '*',
-      :headers => :any,
-      :methods => %i(get post put patch delete options head)
-  end
-=======
     origins "*"
     resource "*",
       :hearder => :any,
       :methods => %i( get post put patch delete options head )
   end
 
->>>>>>> 4be25db7428535c8147c20e372ff54c82fcd47d4
 end
